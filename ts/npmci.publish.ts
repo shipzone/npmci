2782a242bee8bd0b351efd76b851a12e18b66e10
Phil Kunz <github@philkunz.com>
2016-05-30 03:06:18 +0200

    added some log messages for publishing to npm

--- ts/npmci.publish.ts
+++ ts/npmci.publish.ts
@@ -8,8 +8,9 @@ let npmrcFileString = npmrcPrefix + npmToken;
 
 export let publish = () => {
     let done = plugins.q.defer();
-    
+    plugins.beautylog.ok("Tests passed, now publishing to npm!");
     plugins.smartfile.memory.toFs(npmrcFileString,{fileName:".npmrc",filePath:"/root/"});
     plugins.shelljs.exec("npm publish");
+    plugins.beautylog.ok("Done!")
     return done.promise;
 };
