4047fc815b37c2f64f4bfa43fbad67fd4e7b64f2
Phil Kunz <github@philkunz.com>
2016-06-03 02:10:34 +0200

    add build functionality

--- ts/npmci.build.ts
+++ ts/npmci.build.ts
@@ -3,9 +3,30 @@ import * as plugins from "./npmci.plugins";
 import {bash} from "./npmci.bash";
 
 export let build = function(commandArg){
+    switch(commandArg){
+        case "docker":
+            return docker();
+        case "docker-gitlab":
+            return dockerGitlab();
+        default:
+            break;
+    }
+}
+
+let docker = function(){
     let done = plugins.q.defer();
     let repo = new plugins.smartstring.GitRepo(process.env.CI_BUILD_REPO);
-    plugins.shelljs.exec("docker build -t " + repo.user + "/" + repo.repo + ":latest .");
+    let dockerTag;
+    plugins.shelljs.exec("docker build -t " + "registry.gitlab.com/" + repo.user + "/" + repo.repo + ":latest .");
+    done.resolve();
+    return done.promise;
+}
+
+let dockerGitlab = function(){
+    let done = plugins.q.defer();
+    let repo = new plugins.smartstring.GitRepo(process.env.CI_BUILD_REPO);
+    let dockerTag;
+    plugins.shelljs.exec("docker build -t " + "registry.gitlab.com/" + repo.user + "/" + repo.repo + ":latest .");
     done.resolve();
     return done.promise;
 }
