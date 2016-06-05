b84152aac24008bf1a8871ba86bd469fc1e86979
Phil Kunz <github@philkunz.com>
2016-06-05 23:27:19 +0200

    fix typo

--- ts/npmci.paths.ts
+++ ts/npmci.paths.ts
@@ -3,5 +3,5 @@ import * as plugins from "./npmci.plugins";
 
 export let cwd = process.cwd();
 
-export let NpmciPackageRoot = plugins.path.join(__dirname,"../");#
+export let NpmciPackageRoot = plugins.path.join(__dirname,"../");
 export let NpmciPackageConfig = plugins.path.join(NpmciPackageRoot,"./config.json")
