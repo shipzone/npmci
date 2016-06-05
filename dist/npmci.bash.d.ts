1e9c2c850c8f62c444914bd0934f2d62ef5e8c35
Phil Kunz <github@philkunz.com>
2016-06-05 20:51:13 +0200

    added automatic retrial of failed commands

--- dist/npmci.bash.d.ts
+++ dist/npmci.bash.d.ts
@@ -1,3 +1,3 @@
 import "typings-global";
-export declare let bash: (commandArg: any) => void;
-export declare let bashBare: (commandArg: any) => void;
+export declare let bash: (commandArg: string, retryArg?: number) => void;
+export declare let bashBare: (commandArg: any, retryArg?: number) => void;
--- ts/npmci.bash.ts
+++ ts/npmci.bash.ts
@@ -1,25 +1,33 @@
 import "typings-global";
 import * as plugins from "./npmci.plugins";
-export let bash = (commandArg) => {
+export let bash = (commandArg:string,retryArg = 2) => {
     if(!process.env.NPMTS_TEST){
-        let exitCode = plugins.shelljs.exec(
-            "bash -c \"source /usr/local/nvm/nvm.sh &&" +
-            commandArg +
-            "\""
-        ).code;
-        if(exitCode !== 0){
-            process.exit(1);
+        for (let i = 0; i <= retryArg; i++){
+            let exitCode = plugins.shelljs.exec(
+                "bash -c \"source /usr/local/nvm/nvm.sh &&" +
+                commandArg +
+                "\""
+            ).code;
+            if(exitCode !== 0 && i == retryArg){
+                process.exit(1);
+            } else if(exitCode == 0){
+                i = retryArg + 1;
+            }
         }
     } else {
         plugins.beautylog.log("ShellExec would be: " + commandArg.blue)
     }
 }
 
-export let bashBare = (commandArg) => {
+export let bashBare = (commandArg,retryArg = 3) => {
     if (!process.env.NPMTS_TEST){
-        let exitCode = plugins.shelljs.exec(commandArg).code;
-        if(exitCode !== 0){
-            process.exit(1);
+        for(let i = 0; i <= retryArg; i++){
+            let exitCode = plugins.shelljs.exec(commandArg).code;
+            if(exitCode !== 0 && i == retryArg){
+                process.exit(1);
+            } else if(exitCode == 0){
+                i = retryArg + 1;
+            }
         }
     } else {
         plugins.beautylog.log("ShellExec would be: " + commandArg.blue)
