7fc8648d40497b85eb2c8c185e3c1b7c785962b8
Phil Kunz <github@philkunz.com>
2016-06-11 21:01:44 +0200

    now detects wrapped command correctly

--- ts/npmci.command.ts
+++ ts/npmci.command.ts
@@ -4,7 +4,12 @@ import {bash} from "./npmci.bash";
 
 export let command = () => {
     let done = plugins.q.defer()
-    console.log(process.argv);
+    let wrappedCommand:string = "";
+    let argvArray = process.argv;
+    for(let i = 3; i < argvArray.length; i++){
+        wrappedCommand = wrappedCommand + argvArray[i]; 
+    }
+    bash(wrappedCommand);
     done.resolve();
     return done.promise
 }
